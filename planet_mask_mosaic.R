8bc580aff23bb2214024d8c9e36534c3bb04c4a4
Hyena <Hyena@kn-eas-c003.top.orn.mpg.de>
2023-10-04 11:56:16 +0200

    For loops to read in the planet files

---
+++ planet_mask_mosaic.R
@@ -0,0 +1,159 @@
+#K Tiedeman
+#adapted from BCI repository 
+#4 October 2023
+
+
+#The following code uses the Surface Reflectance (SR) Planet Images,
+#Masks the images with the udm2 clip, and then mosaics if there are multiple images for a single day 
+# 
+
+# Set up: 
+#input folder
+indir <- paste0("data/input/")
+
+
+#output folder
+outdir <-'data/planet_masked/'
+#mosaic folder (taking multiple images per day and mosaicking them)\
+
+#install.packages("gdalUtils", repos="http://R-Forge.R-project.org")
+
+#packages 
+library(terra)
+library(dismo)
+library(randomForest)
+library(gdalUtils)
+library(stringr)
+
+
+# This can be commented out, useful for multiple users/computers 
+this <- system('hostname', TRUE)
+if (this == "kn-eas-c003") {
+  dp <- "C:/Users/Hyaena/Documents/planet"
+} else {
+  dp <- "//10.126.19.90/EAS_ind/ktiedeman/data/bci" #
+}
+
+setwd(dp) ### set directory
+#https://developers.planet.com/docs/data/psscene4band/#available-asset-type
+
+###FIRST, A Single image 
+
+sep <- rast("input/PSScene/20230911_153434_97_2484_3B_AnalyticMS_SR_clip.tif")
+#1 blue, 2 green, 3 red , 4 NIR 
+udm2 <- rast("input/PSScene/20230911_153434_97_2484_3B_udm2_clip.tif")
+plot(udm2)
+
+#Udm2 is the quality control - cloud, could shadow and the haze 
+#https://medium.com/planet-stories/finding-the-usable-data-in-planet-imagery-64582d21430e
+#8 layers https://developers.planet.com/docs/data/udm-2/
+
+# Band 1	Clear map	[0, 1]	0: not clear, 1: clear
+# Band 2	Snow map	[0, 1]	0: no snow or ice, 1: snow or ice
+# Band 3	Shadow map	[0, 1]	0: no shadow, 1: shadow
+# Band 4	Light haze map	[0, 1]	0: no light haze, 1: light haze
+# Band 5	Heavy haze map	[0, 1]	0: no heavy haze, 1: heavy haze
+# Band 6	Cloud map	[0, 1]	0: no cloud, 1: cloud
+# Band 7	Confidence map	[0-100]	percentage value: per-pixel algorithmic confidence in classification
+# Band 8	Unusable pixels	--	Equivalent to the UDM asset: see Planet's Imagery Specification for complete details
+
+#From here, the idea 
+idealmask <- udm2[[1]] == 1 & udm2[[2]] == 0 & udm2[[3]]==0 & udm2[[4]]==0 & udm2[[5]]==0 & udm2[[6]] == 0& udm2[[7]] > 70
+idealmask[idealmask==0] <- NA
+#This space for testing. For loops that should be functions below:::##
+SepMasked <- mask(sep, idealmask)
+plot(SepMasked)
+
+plotRGB(SepMasked, r=3, g=2, b=1, stretch = "lin")
+
+
+#####For use with multiple images per day, or multiple downloads#####
+####Mask with planet's udm2 mask in a for loop bc of creative ending....#####
+
+#Repeats from above 
+#input folder
+indir <- paste0("data/input/")
+
+#output folder
+outdir <-'data/planet_masked/'
+
+#Find all the files
+dn <- list.files(path = indir, pattern =  c("*AnalyticMS_clip.tif$|*AnalyticMS_SR_clip.tif$"), recursive = T) # annoying as fuck the ychanged the names again  
+
+dn
+
+j <- str_extract(dn, "[^/]*$")
+outfile <- sub("_3B_AnalyticMS_SR_clip.tif.*", "", j)   
+#Paths 
+pat <- sub("_3B_AnalyticMS_SR_clip.tif.*", "", dn)    
+
+for (i in (1:length(dn))){
+  
+  outname <- paste0(outdir, outfile[i],'_ps4_3B_Analytic_MS_masked', '.tif')
+  print(outname)
+  
+  if(!file.exists(outname)){ #This is to avoid rerunning 
+    rawrast <- rast(paste0(indir,dn[i]))
+    print(paste0(indir,dn[i]))
+    
+    if(file.exists(paste0(indir, pat[i], "_3B_udm2_clip.tif"))){
+      maskj <- rast(paste0(indir, pat[i], "_3B_udm2_clip.tif"))
+      
+      print(paste0(indir, pat[i], "_3B_udm2_clip.tif"))
+      
+      ###Should be a function, but here you can define your confidence level 
+      idealmask <- maskj[[1]] == 1 & maskj[[2]] == 0 & maskj[[3]]==0 & maskj[[4]]==0 & maskj[[5]]==0 & maskj[[6]] == 0 
+      
+    } else {
+      
+      maskj <- rast(paste0(indir, pat[i], "_3B_AnalyticMS_DN_udm_clip.tif"))
+      
+      print(paste0(indir, pat[i], "_3B_AnalyticMS_DN_udm_clip.tif"))
+      
+      idealmask <- maskj < 5 
+      
+    }
+    
+    
+    maskit <- mask(rawrast, idealmask, maskvalue=0, filename = outname, overwrite=T)
+  }
+  
+}
+
+
+#Now you have cloud masked rasters, and for each date you may want to mosaic 
+
+
+
+
+#THis works really well to mosaic the rasters - do we want to cloud mask first and add that as another script????
+
+outMo <-'data/planet_processed/'
+
+
+dn <- list.files(path = outdir, pattern = "*Analytic_MS_masked.tif$", recursive = T)
+j <- str_extract(dn, "[^/]*$") #unneccesary, but if they're in a subfolder helpful 
+
+#Extract just the date itself 
+pa <- str_extract(j, "[^_]+")
+# find the unique dates
+#dt <- unlist(lapply(strsplit(dn, "_"),"[[",1))
+dt <- unlist(unique(pa))
+
+
+for (i in (1:length(dt))){
+  
+  outname <- paste0(outMo, dt[i],'_ps4_3B_Analytic_MS_processed', '.tif')
+  print(outname)
+  if(!file.exists(outname)){
+   # indir2 <- paste0(outdir)
+    rr <- dn[grep(dt[i], dn)]
+    rr <- paste0(outdir, rr)
+    v <- vrt(c(rr)) #https://stackoverflow.com/questions/67169266/error-in-mosaic-of-rasters-from-different-extent-using-terra-package-in-r
+    writeRaster(v, outname, overwrite =T)
+    #mosaic_rasters(gdalfile = rr, dst_dataset = outname, of="GTiff", verbose=TRUE)#, co = list(list("COMPRESS" = "DEFLATE")))
+  }
+  
+}
+
+
